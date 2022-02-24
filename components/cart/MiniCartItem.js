2ca48f0e3471e8eddb443b6c80fc219276a23597
unknown <habibmisi3@gmail.com>
2022-02-24 12:38:11 +0200

    fix images

--- components/cart/MiniCartItem.js
+++ components/cart/MiniCartItem.js
@@ -5,6 +5,7 @@ import cart from '../../api/cart';
 import { connect } from 'react-redux';
 import { $Async } from '../../redux/async_actions';
 import t from '../../helpers/translate';
+import loader from '../../loader';
 
 function CartItem({ src, id, formKey, name, price, quantity, getInputValue }) {
     useEffect(async () => {
@@ -21,7 +22,7 @@ function CartItem({ src, id, formKey, name, price, quantity, getInputValue }) {
                 <i className="fas fa-times"></i>
             </div>
             <div className="picture">
-                <Image width={200} height={350} src={src} className="img" />
+                <Image loader={loader} src={src} width={200} height={350} className="img" />
             </div>
             <div className="details">
                 <span>{name}</span>
